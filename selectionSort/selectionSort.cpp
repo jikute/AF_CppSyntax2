d29823f9d31f32b2421c9445487dd9c1e8ea0ce0
jikute <54138884+jikute@users.noreply.github.com>
2021-02-23 11:04:16 -0800

    2/23

--- selectionSort/selectionSort.cpp
+++ selectionSort/selectionSort.cpp
@@ -1,6 +1,37 @@
 #include<stdio.h>
 
+void selectionSort(int a[], int n);
+
+// test the function
 int main()
 {
+	int a[9] = { 3,4,3,1,200,399,333,22,90 };
+	selectionSort(a, 9);
 	return 0;
+}
+
+// selection sort method to sort the sequence from small to large
+void selectionSort(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//save the current position
+		int position = i;
+		for (int j = i; j < n; j++)
+		{
+			//find the smallest value's position from \
+			current position to the end of the array.
+			if (a[position] > a[j])
+			{
+				position = j;
+			}
+		}
+		// move the smallest value to the left of the array.
+		if (position != i)
+		{
+			int temp = a[i];
+			a[i] = a[position];
+			a[position] = temp;
+		}
+	}
 }
